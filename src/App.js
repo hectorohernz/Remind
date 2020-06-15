3d294d82535925ffb1673c4356068575d9432d0b
Hector Hernandez <hectorohernz@gmail.com>
2020-06-15 15:35:28 -0400

    removed contact

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ class App extends React.Component{
     return (
       <section className="App">
         <ToList/>
-        <div className="contact">
-          <a href="https://github.com/hectorohernz" target="_blank"><i className="fab fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/hector-hernandez-383444196" target="_blank"><i className="fab fa-linkedin"></i></a>
-        </div>
       </section>
     )
   }
